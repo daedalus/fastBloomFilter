4582e89da27e291f4f37bae8a673200103a6daf4
Dario Clavijo <daedalus2027@gmail.com>
2017-04-26 00:34:01 -0300

    mkbloom.py update

--- mkbloom.py
+++ mkbloom.py
@@ -1,13 +1,12 @@
 import sys
-import zlib
-import bz2
-import lz4
-import brotli
-import hashlib
-
 import bloom
 
-bf = bloom.BloomFilter(array_size=(1024**3)*5,do_bkp=False, bitshuffle=True)
+try:
+	array_size = int(sys.argv[2])
+except:
+	array_size = (1024**3)*5
+
+bf = bloom.BloomFilter(array_size=array_size,do_bkp=False, bitshuffle=True)
 bf.filename = sys.argv[1]
 bf.save()
 
