0174d41ff4869b4bcbc51592c61e813b007ca086
Dario Clavijo <daedalus2027@gmail.com>
2017-05-24 13:25:45 -0300

    loadbloom test

---
+++ loadbloom.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# Author Dario Clavijo 2017
+# GPlv3
+
+import bloom
+import fileinput
+import sys
+
+bf = bloom.BloomFilter()
+
+fp = open(sys.argv[1],'r')
+for line in fp:
+	bf.add(line.rstrip())
+fp.close()
+
+bf.save(sys.argv[2])
+
--- mkbloom.py
+++ mkbloom.py
@@ -4,9 +4,9 @@ import bloom
 try:
 	array_size = int(sys.argv[2])
 except:
-	array_size = (1024**3)*5
+	array_size = (1024**3)*1
 
-bf = bloom.BloomFilter(array_size=array_size,do_bkp=False,do_hashing=False, bitshuffle=False)
-bf.filename = sys.argv[1]
-bf.save()
+bf = bloom.BloomFilter(array_size=array_size,do_bkp=False,do_hashing=False, bitshuffle=False,fast=False)
+filename = sys.argv[1]
+bf.save(filename)
 
