f6ecb0aafc958b46dc32f3bc83e67cc4cbab36dc
Darío Clavijo <daedalus2027@gmail.com>
2017-10-14 00:27:48 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+VERSION = '0.0.1'
+BASE_CVS_URL = 'http://github.com/daedalus/bloomfilter'
+
+setup(
+    name='bloomfilter',
+    packages=['bloomfilter', ],
+    version=VERSION,
+    author='Dario Clavijo',
+    author_email='dclavijo@protonmail.com',
+    install_requires=[x.strip() for x in open('requirements.txt').readlines()],
+    url=BASE_CVS_URL,
+    download_url='{}/tarball/{}'.format(BASE_CVS_URL, VERSION),
+    test_suite='tests',
+    tests_require=[x.strip() for x in open('requirements_test.txt').readlines()],
+    keywords=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+    ],
+)
