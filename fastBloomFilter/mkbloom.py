2e1d1fb71f5888beaf71973b6900c5fcf725f637
Darío Clavijo <daedalus2027@gmail.com>
2017-10-28 02:51:49 -0300

    trailing whitespace

--- fastBloomFilter/mkbloom.py
+++ fastBloomFilter/mkbloom.py
@@ -7,7 +7,7 @@ try:
 	Gigs = int(sys.argv[2])
 except ValueError as verr:
 	print "Plase input the correct number of Gigabytes of RAM to be used."
-	exit(1)	
+	exit(1)
 
 if Gigs > 0:
 	bf = bloom.BloomFilter(array_size=Gigs*(1024**3),do_bkp=False,do_hashing=False,fast=False)
