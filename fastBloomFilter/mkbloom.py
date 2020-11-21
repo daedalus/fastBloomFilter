31ec169cb95d94ec70c495655c82e82dc62e1ef7
Darío Clavijo <clavijodario@gmail.com>
2020-11-20 23:42:53 -0300

    Update mkbloom.py

--- fastBloomFilter/mkbloom.py
+++ fastBloomFilter/mkbloom.py
@@ -6,7 +6,7 @@ filename = sys.argv[1]
 try:
 	Gigs = int(sys.argv[2])
 except ValueError as verr:
-	print "Plase input the correct number of Gigabytes of RAM to be used."
+	print("Plase input the correct number of Gigabytes of RAM to be used.")
 	exit(1)
 
 if Gigs > 0:
