5b201095bc235927cebdbc0798ece42bf817550f
Darío Clavijo <clavijodario@gmail.com>
2020-11-20 23:42:06 -0300

    Update test.py

--- test.py
+++ test.py
@@ -6,10 +6,9 @@ bf.add('30000')
 bf.add('1230213') 
 bf.add('1')
 
-print bf.update('1') 
-print bf.update('2') 
-
-print bf.query('1')
+print(bf.update('1'))
+print(bf.update('2')) 
+print(bf.query('1'))
 
 bf.stat()
 bf.info()
